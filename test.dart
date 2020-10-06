f50d72e28e8c2d73ada00fe23ce691da80f35b93
Seyit Ömer Karataş <29843189+hector003@users.noreply.github.com>
2020-10-06 11:33:24 +0300

    denemeler

--- test.dart
+++ test.dart
@@ -1,10 +1,12 @@
 main(List<String> args) {
 
-  
-
   print("selam");
+  int yas = 123;
+  yas +=12;
+  print(yas);
   print("selam ");
-
-
-
+  num numara = 123;
+  num ddd = 123.34;
+  String ad = "sssss";
+  double aaaa = 154.4;
 }
